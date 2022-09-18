02f4ac30fd5e05c188d305581e269adc1392bd04
Nitya <nityaagarwal0218@gmail.com>
2022-09-18 15:18:42 +0530

    create a UserRepository interface  in dao package which extends CrudRepository

---
+++ CRUDApp/src/main/java/com/mycompany/crudapp/dao/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mycompany.crudapp.dao;
+
+import com.mycompany.crudapp.CrudAppApplication;
+import com.mycompany.crudapp.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Integer>{
+
+}
