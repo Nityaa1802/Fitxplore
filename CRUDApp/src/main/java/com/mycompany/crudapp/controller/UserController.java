42f0817df98fd96fe3c93c6dba80dfa109275868
Nitya <nityaagarwal0218@gmail.com>
2022-12-21 18:29:55 +0530

    add the handler method to shoew last page and ended the project

--- CRUDApp/src/main/java/com/mycompany/crudapp/controller/UserController.java
+++ CRUDApp/src/main/java/com/mycompany/crudapp/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.mycompany.crudapp.controller;
 
-import com.mycompany.crudapp.model.User;
+import com.mycompany.crudapp.user.User;
 import com.mycompany.crudapp.service.UserNotFoundException;
 import com.mycompany.crudapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,50 +14,62 @@ import java.util.List;
 @Controller
 public class UserController {
     @Autowired
-    private UserService userService;
+    private UserService service;
+
+    // now code the handler method to handle the request of viewing the list of users
 
     @GetMapping("/users")
-    public String showUserList(Model model){
-        List<User> users=userService.listAll();
-        model.addAttribute("listUsers",users);
+    public String showUsersList(Model model) {
+
+        List<User> listUsers = service.listAll();
+        model.addAttribute("listUsers", listUsers);
         return "users";
-    }
-       @GetMapping("/users/new")
-    public String showNewForm(Model model){
-        model.addAttribute("user",new User());
-        model.addAttribute("pageTitle","Add New User");
-        return "user_form";
+
     }
 
-    @PostMapping("/user/save")
-    public String saveUser(User user, RedirectAttributes ra){
-        userService.save(user);
-        ra.addFlashAttribute("message","The User has been saved successfully.");
+    @GetMapping("/users/new")
+    public String showNewForm(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("pageTitle", "Add New User");
+        return "user_form";
+
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+        service.save(user);
+        redirectAttributes.addFlashAttribute("message", "The User Has Been Saved Successfully");
+
+
         return "redirect:/users";
+
     }
 
     @GetMapping("/users/edit/{id}")
-    public String showEditForm(@PathVariable("id")Integer id,Model model,RedirectAttributes ra){
+    public String showEditForm(@PathVariable("id") Integer id, Model model, RedirectAttributes redirectAttributes) {
         try {
-            User user=userService.get(id);
-            model.addAttribute("user",user);
-            model.addAttribute("pageTitle","Edit User (ID: "+id+")");
+            User user = service.get(id);
+            model.addAttribute("user", user);
+            model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+
             return "user_form";
         } catch (UserNotFoundException e) {
-            ra.addFlashAttribute("message",e.getMessage());
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
+
             return "redirect:/users";
         }
     }
 
-    @DeleteMapping("/user/delete/{id}")
-    public String deleteUser(@PathVariable("id")Integer id,Model model,RedirectAttributes ra){
+    @GetMapping("/users/delete/{id}")
+    public String deleteUser(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes) {
         try {
-            userService.delete(id);
-            ra.addFlashAttribute("message","User ID"+id+"has been deleted.");
+            service.delete(id);
+            redirectAttributes.addFlashAttribute("message", "The user id " + id + " has been successfully deleted");
         } catch (UserNotFoundException e) {
-            ra.addFlashAttribute("message",e.getMessage());
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
+
+
         }
         return "redirect:/users";
     }
-
 }
--- CRUDApp/src/main/java/com/mycompany/crudapp/dao/UserRepository.java
+++ CRUDApp/src/main/java/com/mycompany/crudapp/dao/UserRepository.java
@@ -1,7 +1,6 @@
 package com.mycompany.crudapp.dao;
 
-import com.mycompany.crudapp.CrudAppApplication;
-import com.mycompany.crudapp.model.User;
+import com.mycompany.crudapp.user.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User,Integer>{
